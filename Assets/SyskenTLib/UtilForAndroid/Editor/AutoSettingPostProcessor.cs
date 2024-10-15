84bc551d262d503ccdf599fc8a7c04be456fed81
ppengotsu <ppengotsugihub@sysken.tokyo>
2024-10-15 22:57:43 +0900

    コミット漏れ

--- Assets/SyskenTLib/UtilForAndroid/Editor/AutoSettingPostProcessor.cs
+++ Assets/SyskenTLib/UtilForAndroid/Editor/AutoSettingPostProcessor.cs
@@ -1,26 +1,31 @@
 using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
 using UnityEditor.Callbacks;
 using UnityEngine;
 
 namespace SyskenTLib.UtilForAndroid.Editor
 {
-    public class AutoSettingPostProcessor
+    public class AutoSettingPostProcessor:IPreprocessBuildWithReport, IPostprocessBuildWithReport
     {
+        // 実行順
+        public int callbackOrder => 1;
         
-        [PostProcessBuild]
-        public static void OnPostProcessBuild(BuildTarget buildTarget, string path)
-        {
 
-            if (buildTarget == BuildTarget.Android)
+        // 事前実行
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            Debug.Log("OnPreprocessBuild　バンドルバージョンの処理");
+            if (report.summary.platform == BuildTarget.Android)
             {
                 
                 //
                 // パス系を控える処理
                 //
-                Debug.Log("APKパス："+path);
+                Debug.Log("APKパス："+report.summary.outputPath);
 
                 UtilForAndroidManager utilForAndroidManager = new UtilForAndroidManager();
-                utilForAndroidManager.SaveLastBuildAPKPath(path);
+                utilForAndroidManager.SaveLastBuildAPKPath(report.summary.outputPath);
                 
                 
                 string currentAppID = PlayerSettings.GetApplicationIdentifier(BuildTargetGroup.Android);
@@ -34,8 +39,18 @@ namespace SyskenTLib.UtilForAndroid.Editor
                 AndroidBuildManager androidBuildManager = new AndroidBuildManager();
                 androidBuildManager.StartBuildCustom();
                 
-                
+                AssetDatabase.Refresh (); // アセットDBの更新
             }
         }
+        
+        /// <summary>
+        /// ビルド後
+        /// </summary>
+        /// <param name="report"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void OnPostprocessBuild(BuildReport report)
+        {
+        }
+
     }
 }
